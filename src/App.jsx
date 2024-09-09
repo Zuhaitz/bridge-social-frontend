b88988f55c337ca040dafde751eb11210910cb9c
Zuhaitz <zuhaitzmartinezjimenez@gmail.com>
2024-09-09 20:20:12 +0200

    Routing immpllemented

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,22 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Home from "./pages/home/Home";
+
 function App() {
-  return <></>;
+  return (
+    <BrowserRouter>
+      <section className="layout">
+        <header className="header">{/* <Navbar /> */}</header>
+
+        <main className="main">
+          <Routes>
+            <Route path="/" element={<Home />} />
+          </Routes>
+        </main>
+
+        <footer className="footer">{/* <Footer /> */}</footer>
+      </section>
+    </BrowserRouter>
+  );
 }
 
 export default App;
