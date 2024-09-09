72a63fb1acc7d7c764f2540b2c22da9e3c1ccffd
Zuhaitz <zuhaitzmartinezjimenez@gmail.com>
2024-09-09 20:23:13 +0200

    navbar and footer added

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,16 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
+
 import Home from "./pages/home/Home";
+import Navbar from "./components/navbar/Navbar";
+import Footer from "./components/footer/Footer";
 
 function App() {
   return (
     <BrowserRouter>
       <section className="layout">
-        <header className="header">{/* <Navbar /> */}</header>
+        <header className="header">
+          <Navbar />
+        </header>
 
         <main className="main">
           <Routes>
@@ -13,7 +18,9 @@ function App() {
           </Routes>
         </main>
 
-        <footer className="footer">{/* <Footer /> */}</footer>
+        <footer className="footer">
+          <Footer />
+        </footer>
       </section>
     </BrowserRouter>
   );
