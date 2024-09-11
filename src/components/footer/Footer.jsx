be697ff4ac7e5227009f59d47c87f3dbae98a43c
Zuhaitz <zuhaitzmartinezjimenez@gmail.com>
2024-09-11 19:52:26 +0200

    footer done

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,5 +1,24 @@
+import "./Footer.scss";
+
 const Footer = () => {
-  return <div>Footer</div>;
+  return (
+    <div className="foot">
+      <p className="foot__legal">
+        Made for <span className="foot__highlight">The Bridge</span>
+      </p>
+
+      <p className="foot__credits">
+        Made by{" "}
+        <a
+          href="https://github.com/Zuhaitz"
+          target="_blank"
+          className="foot__link"
+        >
+          Zuhaitz
+        </a>
+      </p>
+    </div>
+  );
 };
 
 export default Footer;
