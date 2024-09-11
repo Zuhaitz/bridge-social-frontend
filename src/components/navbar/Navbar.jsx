4207853b95dcc498f6b49400f1e9aa742973e498
Zuhaitz Martínez <zuhaitzmartinezjimenez@gmail.com>
2024-09-11 14:25:18 +0200

    some style in navbar

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,5 +1,24 @@
+import "./Navbar.scss";
+
 const Navbar = () => {
-  return <section className="sidebar">Navbar</section>;
+  return (
+    <section className="navbar sidebar">
+      <p className="navbar__title">
+        So<span className="text__color">cia</span>l Net
+        <span className="text__color">work</span>
+      </p>
+      <div className="navbar__links">
+        <a href="/" className="navbar__link">
+          Home
+        </a>
+        <a href="/profile" className="navbar__link">
+          Profile
+        </a>
+      </div>
+
+      <button className="navbar__button">Create post</button>
+    </section>
+  );
 };
 
 export default Navbar;
--- src/components/sidebar/Sidebar.jsx
+++ src/components/sidebar/Sidebar.jsx
@@ -1,5 +1,5 @@
 const Sidebar = () => {
-  return <section className="sidebar">Sidebar</section>;
+  return <section className="sidebar"></section>;
 };
 
 export default Sidebar;
