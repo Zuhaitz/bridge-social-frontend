e6206106e33de8de6529eca2f80a4514fc962884
Zuhaitz Martínez <zuhaitzmartinezjimenez@gmail.com>
2024-09-11 13:03:42 +0200

    page styling change

--- src/App.jsx
+++ src/App.jsx
@@ -3,20 +3,27 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Home from "./pages/home/Home";
 import Navbar from "./components/navbar/Navbar";
 import Footer from "./components/footer/Footer";
+import Sidebar from "./components/sidebar/Sidebar";
 
 function App() {
   return (
     <BrowserRouter>
       <section className="layout">
-        <header className="header">
-          <Navbar />
-        </header>
-
-        <main className="main">
-          <Routes>
-            <Route path="/" element={<Home />} />
-          </Routes>
-        </main>
+        <div className="page">
+          <header className="header">
+            <Navbar />
+          </header>
+          <main className="main">
+            <div className="container">
+              <Routes>
+                <Route path="/" element={<Home />} />
+              </Routes>
+            </div>
+            <div>
+              <Sidebar />
+            </div>
+          </main>
+        </div>
 
         <footer className="footer">
           <Footer />
