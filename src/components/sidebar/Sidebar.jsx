876f647a99205420bf847ff27cef808b653b0d06
Zuhaitz <zuhaitzmartinezjimenez@gmail.com>
2024-09-13 20:02:03 +0200

    search bar style done

--- src/components/sidebar/Sidebar.jsx
+++ src/components/sidebar/Sidebar.jsx
@@ -1,10 +1,15 @@
 import { useState } from "react";
 import searchIcon from "../../assets/icons/search.svg";
+import crossIcon from "../../assets/icons/cross-small.svg";
 
 import "./Sidebar.scss";
 
 const Sidebar = () => {
   const [search, setSearch] = useState("");
+
+  const onCleanSearch = () => {
+    setSearch("");
+  };
 
   return (
     <section className="sidebar rightbar">
@@ -14,11 +19,17 @@ const Sidebar = () => {
           type="text"
           placeholder="Search"
           value={search}
+          spellCheck="false"
           onChange={({ target }) => setSearch(target.value)}
           className="rightbar__input"
         />
-        <button className="rightbar__searchEmpty">
-          <p>x</p>
+        <button
+          onClick={onCleanSearch}
+          className={`rightbar__cleanSearch ${
+            search && "rightbar__cleanSearch--visible"
+          }`}
+        >
+          <img src={crossIcon} alt="clean search icon" />
         </button>
       </div>
     </section>
