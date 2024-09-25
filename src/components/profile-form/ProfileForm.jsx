c9d38111d3d3b46ea2648da19dc3d9f71a8299d7
Zuhaitz <zuhaitzmartinezjimenez@gmail.com>
2024-09-25 18:35:00 +0200

    profile upload menu done

--- src/components/profile-form/ProfileForm.jsx
+++ src/components/profile-form/ProfileForm.jsx
@@ -1,10 +1,27 @@
-import React from "react";
+import { useState } from "react";
 
 import cameraIcon from "../../assets/icons/camera.svg";
 
 import "./ProfileForm.scss";
 
 const ProfileForm = ({ setEditing, profile }) => {
+  const [banner, setBanner] = useState(profile.banner);
+  const [picture, setPicture] = useState(profile.picture);
+
+  const updateBanner = async (event) => {
+    if (event.target.files.length === 0) return;
+    setBanner(URL.createObjectURL(event.target.files[0]));
+  };
+
+  const updatePicture = async (event) => {
+    if (event.target.files.length === 0) return;
+    setPicture(URL.createObjectURL(event.target.files[0]));
+  };
+
+  const onSaveChanges = (event) => {
+    event.preventDefault();
+  };
+
   const onCancel = (event) => {
     event.preventDefault();
     setEditing(false);
@@ -15,21 +32,44 @@ const ProfileForm = ({ setEditing, profile }) => {
       <p className="profile-form__title">Edit my profile</p>
 
       <div className="profile-form__image">
-        <div className="profile-form__banner">
-          {profile.banner && <img src={profile.banner} alt="user banner" />}
-        </div>
-        <div className="profile-form__icon">
-          <img src={cameraIcon} alt="change banner icon" />
-        </div>
+        <label htmlFor="banner">
+          <div className="profile-form__banner">
+            {banner && <img src={banner} alt="user banner" />}
+          </div>
+
+          <div className="profile-form__icon">
+            <img src={cameraIcon} alt="change banner icon" />
+          </div>
+        </label>
+
+        <input
+          id="banner"
+          type="file"
+          accept="image/png, image/jpeg"
+          onInput={updateBanner}
+          className="profile-form__file-input"
+        />
       </div>
 
       <div className="profile-form__container">
-        <div className="profile-form__picture">
-          <img src={profile.picture || userIcon} alt="user profile picture" />
-        </div>
+        <label htmlFor="picture">
+          <div className="profile-form__picture">
+            <img src={picture || userIcon} alt="user profile picture" />
+          </div>
+        </label>
+
+        <input
+          id="picture"
+          type="file"
+          accept="image/png, image/jpeg"
+          onInput={updatePicture}
+          className="profile-form__file-input"
+        />
 
         <div className="profile-form__buttons">
-          <button className="profile-form__button">Save changes</button>
+          <button onClick={onSaveChanges} className="profile-form__button">
+            Save changes
+          </button>
           <button
             onClick={onCancel}
             className="profile-form__button profile-form__button--cancel"
